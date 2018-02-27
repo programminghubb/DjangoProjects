1ce7ee4211cd5e35f497b44e02f7413bfd54e541
programminghubb <35744285+programminghubb@users.noreply.github.com>
2018-02-27 21:50:49 +0545

    creating registration/models.py

--- mywebapplication/registration/models.py
+++ mywebapplication/registration/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class SignUp(models.Model):
+    first_name = models.CharField(max_length=30, blank=False, help_text='Insert your First Name')
+    last_name = models.CharField(max_length=30, blank=False, help_text='Insert your Last Name')
+    email = models.EmailField(max_length=254, blank=False, help_text='Insert a valid email address.')
+    password = models.CharField(max_length=24, blank=False, help_text='Insert new password')
+
+
--- mywebapplication/registration/views.py
+++ mywebapplication/registration/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
 
 def sign_up(request):
     return render(request, 'registration/sign_up.html')
